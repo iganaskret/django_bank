3a7adaf22c449545be81e165a429d49608d23877
Ksawery <ksawery@superherocheesecake.com>
2021-05-31 15:17:50 +0200

    do not use

--- notifier/apps.py
+++ notifier/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class NotifierConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifier'
-
-    def ready(self):
-        from . import signals
--- notifier/consumers.py
+++ notifier/consumers.py
@@ -5,9 +5,9 @@ from time import sleep
 from channels.generic.websocket import WebsocketConsumer
 from channels.generic.websocket import AsyncConsumer
 from channels.db import database_sync_to_async
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
+<<<<<<< HEAD
 class NotifierConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
@@ -22,3 +22,38 @@ class NotifierConsumer(AsyncJsonWebsocketConsumer):
     async def user_gossip(self, event):
         await self.send_json(event)
         print(f"Got message {event} at {self.channel_name}")
+=======
+# class NotifierConsumer(WebsocketConsumer):
+#     def connect(self, event):
+#         # accept incoming connection from the browser(client)
+#         self.accept()
+#         # message to the connection as json object
+#         for i in range(1000):
+#             # send method
+#             self.send(json.dumps({
+#                 # message key
+#                 'message': randint(1, 10)
+#             }))
+#             sleep(2)
+
+class NotifierConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        print("connected", event)
+        # print(self.scope)
+        await self.send({
+            "type": "websocket.accept"
+        })
+
+        #other_user = self.scope['url_route']
+        await self.send({
+            "type": "websocket.send",
+            "text": "Hello"
+        })
+
+    async def websocket_receive(self, event):
+        print("receive", event)
+        #print(f'EVENT {event}')
+
+    async def websocket_disconnect(self, event):
+        print("disconnect", event)
+>>>>>>> parent of 34c60b1... user notification works

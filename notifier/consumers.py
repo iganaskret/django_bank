739a8a6b75054a9fe6315e7a03400be557b975be
Ksawery <ksawery@superherocheesecake.com>
2021-05-31 11:15:43 +0200

    channel notifier works / add routing

--- notifier/consumers.py
+++ notifier/consumers.py
@@ -8,58 +8,16 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-# class NotifierConsumer(WebsocketConsumer):
-#     def connect(self, event):
-#         # accept incoming connection from the browser(client)
-#         self.accept()
-#         # message to the connection as json object
-#         for i in range(1000):
-#             # send method
-#             self.send(json.dumps({
-#                 # message key
-#                 'message': randint(1, 10)
-#             }))
-#             sleep(2)
-
-# class NotifierConsumer(AsyncConsumer):
-#     async def websocket_connect(self, event):
-#         print("connected", event)
-#         await self.send({
-#             "type": "websocket.accept"
-#         })
-
-#         await self.send({
-#             "type": "websocket.send",
-#             "text": "Hello"
-#         })
-
-#     async def websocket_receive(self, event):
-#         print("receive", event)
-
-#     async def websocket_disconnect(self, event):
-#         print("disconnect", event)
-
-
-# class NotifierConsumer(AsyncJsonWebsocketConsumer):
-
-#     async def connect(self):
-#         await self.accept()
-#         while 1:
-#             await asyncio.sleep(1)
-#             await self.send_json("tick")
-#             await asyncio.sleep(1)
-#             await self.send_json(".....tock")
-
 class NotifierConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
         await self.accept()
-        await self.channel_layer.group_add("gossip", self.channel_name)
-        print(f"Added {self.channel_name} channel to gossip")
+        await self.channel_layer.group_add("notify", self.channel_name)
+        print(f"Added {self.channel_name} channel to notify")
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard("gossip", self.channel_name)
-        print(f"Removed {self.channel_name} channel to gossip")
+        await self.channel_layer.group_discard("notify", self.channel_name)
+        print(f"Removed {self.channel_name} channel to notify")
 
     async def user_gossip(self, event):
         await self.send_json(event)
